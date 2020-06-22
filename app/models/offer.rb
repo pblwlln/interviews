214bbf20f8daa28c92408d0d6d03be350561c3c6
Pablo Willian Suchewschy <pblwlln@gmail.com>
2020-06-21 22:30:07 -0300

    URL validation

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,6 @@
 class Offer < ApplicationRecord
   validates :advertiser_name, presence: true, uniqueness: true
-  validates :url, presence: true
+  validates :url, presence: true, url: true
   validates :description, presence: true, length: { maximum: 500 }
   validates :starts_at, presence: true
 end
